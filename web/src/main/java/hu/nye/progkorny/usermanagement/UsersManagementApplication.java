43948fe3338434c7efb72a16095bacf5ffde4d97
SZABICS <szabics.norbert96@gmail.com>
2022-05-15 17:10:33 +0200

    testt

---
+++ web/src/main/java/hu/nye/progkorny/usermanagement/UsersManagementApplication.java
@@ -0,0 +1,11 @@
+package hu.nye.progkorny.usermanagement;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersManagementApplication {
+
+	public static void main(final String[] args) {
+
+	}
+}
---
+++ web/src/test/java/hu/nye/progkorny/usermanagement/UsersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkorny.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
