8b4e9dc19bd1dd515d61bd67db6b8022083fa50e
SZABICS <szabics.norbert96@gmail.com>
2022-05-15 16:20:41 +0200

    Project init

---
+++ src/test/java/hu/nye/progkorny/usermanagement/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkorny.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
